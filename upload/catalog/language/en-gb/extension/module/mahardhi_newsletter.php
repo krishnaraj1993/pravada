f4e88f3be48e66beb595624b0cec54bc1924c403
krish <kpattar45@gmail.com>
2020-10-26 22:59:50 +0530

    first commit

---
+++ upload/catalog/language/en-gb/extension/module/mahardhi_newsletter.php
@@ -0,0 +1,19 @@
+<?php
+
+$_['newsletter_title']        = 'Sign Up For Newsletter';
+$_['newsletter_description']  = 'Get e-mail updates about our  latest shop and special offers.';
+
+$_['newsletter_title_popup']        = "Newsletter";
+$_['newsletter_description_popup']  = 'signup for our newsletter to stay up-to-date with our promotions,discounts,sales, and special offers.';
+
+$_['entry_user_email']  	  = 'Enter E-mail Hear...';
+$_['entry_subscribe']		  =  'Subscribe';
+$_['entry_unsubscribe']		  =  'Unsubscribe';
+$_['entry_show_again']  			= 'Don\'t show this popup again!';
+
+//Messages
+$_['error_message'] 	 	= 'Error : Please enter valid email address.';
+$_['success_subscribe']	 	= 'Success : Subscribed Successfully.';
+$_['success_unsubscribe'] 	= 'Success : Unsubscribed Successfully.';
+$_['already_subscribe'] 	= 'Error : Your Email Address already exist.';
+$_['error_exist'] 	    	= 'Error : Your Email Address doesn\'t exist.';
---
+++ upload/catalog/language/en-gb/extension/module/mahardhi_product_tab.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Our Products';
+
+$_['tab_latest']       = 'ALL';
+$_['tab_bestseller']   = 'Plates';
+$_['tab_featured']     = 'Cutlery';
+$_['tab_special']      = 'Bowls';
+
+// Text
+$_['button_wishlist']  = 'Add to wishlist';
+$_['button_compare']   = 'Compare';
+$_['text_tax']         = 'Without tax:';
+?>
